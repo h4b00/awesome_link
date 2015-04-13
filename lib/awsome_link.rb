09298797994d870e39b5fd17bfe812f4cd5515a9
h4b0 <h4b0@h4b0.pl>
2015-04-13 21:51:28 +0200

    Fixed function

--- lib/awsome_link.rb
+++ lib/awsome_link.rb
@@ -1,12 +1,12 @@
 module AwsomeLink
   def awsome_link(icon_class = nil, url_options = nil, html_options = nil, &block)
-    link_to("#{ icon_tag(icon_class, html_options) }".html_safe, url_options, &block)
+    link_to("#{ icon_tag(icon_class) }".html_safe, url_options, html_options, &block)
   end
 
   private
 
-  def icon_tag(icon_class, style)
-    "<i class='fa-#{ icon_class }' #{style}></i>"
+  def icon_tag(icon_class)
+    "<i class='fa-#{ icon_class }'></i>"
   end
 end
 
